0d11357873a9d2b811222e091ea8f9984562fb43
Tim K-G <tim.theodor@gmail.com>
2020-03-03 14:35:10 +0100

    Adds syntax highlighting and nav to React page

--- src/pages/react.js
+++ src/pages/react.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Nav from "../nav";
 
 class ContrivedExample extends React.Component {
     render() {
@@ -12,15 +13,27 @@ class ContrivedExample extends React.Component {
 
 export default class ReactPage extends Component {
 
+    componentDidMount() {
+        document.querySelectorAll('pre code').forEach((block) => {
+            window.hljs.highlightBlock(block);
+          });
+    }
+
     render() {
         return (
             <article>
+                <Nav links={[
+                    ["#components", "Components and Elements"]
+                ]}></Nav>
                 <h1>React</h1>
                 <p>
                     React is a library for building User Interfaces. You can think of a React application as a function
                     which receives data as input and produces your UI as output. Things like data fetching, advanced state management,
                     routing, etc. are outside of the scope of React itself.
                 </p>
+
+                <h2 id="components">Components and Elements</h2>
+
                 <p>
                     React abstracts away DOM manipulation. You won't create DOM nodes and attach and remove event handlers.
                     Instead, you'll write your own Components - UI structures with lifecycle methods and data handling - and 
@@ -30,25 +43,27 @@ export default class ReactPage extends Component {
                 </p>
 
                 <pre className="code">
-{`
-    // defining a component...
-    class ContrivedExample extends React.Component {
-        render() {
-            const person = this.props.person;
+                <code className="javascript">
+                {`
+// defining a component...
+class ContrivedExample extends React.Component {
+    render() {
+        const person = this.props.person;
 
-            return (
-                React.createElement("p", {}, \`Hi \${person}, I'm a contrived example!\`)
-            );
-        }
+        return (
+            React.createElement("p", {}, \`Hi \${person}, I'm a contrived example!\`)
+        );
     }
+}
 
-    // and creating a representation of how we want our App to look like
-    const contrivedExample = 
-        React.createElement(ContrivedExample, { person: "Visitor" }, [])
+// and creating a representation of how we want our App to look like
+const contrivedExample = 
+    React.createElement(ContrivedExample, { person: "Visitor" }, [])
 
-    // and injecting our "app" into the page
-    ReactDOM.render(contrivedExample, document.body)
+// and injecting our "app" into the page
+ReactDOM.render(contrivedExample, document.body)
 `}
+                </code>
                 </pre>
 
                 <p>
@@ -61,35 +76,36 @@ export default class ReactPage extends Component {
 
                 <p>
                     We have seen a few things in the example above:
-                    <ol>
-                        <li>
-                            We define our custom UI pieces as Components with a render function,
-                            which returns React Elements
-                        </li>
-                        <li>
-                            React Elements are used to represent simple DOM tags as well as 
-                            our custom, more complex Components
-                        </li>
-                        <li>
-                            We never instantiate DOM elements directly - we only create React Elements whose <em>type</em>
-                            (the first argument to React.createElement) is a string of the DOM element we want
-                        </li>
-                        <li>
-                            In order to instruct React to use our custom components, we simply call React.createElement with a 
-                            reference to our custom Component.
-                        </li>
-                        <li>
-                            We can pass data to our Components using props.
-                        </li>
-                        <li>
-                            Our job as UI developer consists of writing the components we need and assembling
-                            them together as a tree of React Elements.
-                        </li>
-                        <li>
-                            The actual rendering and DOM manipulation of the app is done by ReactDOM.
-                        </li>
-                    </ol>
                 </p>
+                <ol>
+                    <li>
+                        We define our custom UI pieces as Components with a render function,
+                        which returns React Elements
+                    </li>
+                    <li>
+                        React Elements are used to represent simple DOM tags as well as 
+                        our custom, more complex Components
+                    </li>
+                    <li>
+                        We never instantiate DOM elements directly - we only create React Elements whose <em>type</em>
+                        (the first argument to React.createElement) is a string of the DOM element we want
+                    </li>
+                    <li>
+                        In order to instruct React to use our custom components, we simply call React.createElement with a 
+                        reference to our custom Component.
+                    </li>
+                    <li>
+                        We can pass data to our Components using props.
+                    </li>
+                    <li>
+                        Our job as UI developer consists of writing the components we need and assembling
+                        them together as a tree of React Elements.
+                    </li>
+                    <li>
+                        The actual rendering and DOM manipulation of the app is done by ReactDOM.
+                    </li>
+                </ol>
+                
 
                 <p>
                     This concludes our first look at React. Next we'll have a look at React's infamous JSX templating library.
