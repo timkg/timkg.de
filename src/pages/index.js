329eb2268325de4fe8c507ca66d4d1f17c0f3a94
Tim K-G <tim.theodor@gmail.com>
2020-03-05 17:25:22 +0100

    editing of intro

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,17 +9,17 @@ export default class IndexPage extends Component {
                 <h1>What the Gatsby</h1>
                 <p>
                     I've been a frontend developer for most of my career. 
-                    And my career has been going for quite a few years now.
+                    And my career has been going on for quite a few years now.
                     I started with jQuery, migrated to Backbone.js,
                     tried out Angular, and settled on React.js. I know my way around Node.js, use Webpack,
                     and consider myself quite proficient at what I do.
                 </p>
                 <p>
                     Recently I wanted to try out Gatsby.js, a supposedly "static site generator". Well,
-                    it generates static sites. But I was flabbergasted by all of the stuff which 
-                    kind of happens automatically. Don't get me wrong. Using tools which automate repetitive tasks is 
-                    a good thing. However, things become less simple when we start using 
-                    tools which automate stuff we don't really understand. So I decided to start breaking down
+                    it generates static sites. But I was almost overwhelmed by all the things which 
+                    Gatsby does for you automatically. Don't get me wrong. Using tools which automate repetitive tasks is 
+                    a good thing. However, using tools without understanding how they work 
+                    is risky business. So I decided to start breaking down
                     what Gatsby actually does, and explore these concepts separately, one at a time.
                 </p>
                 <p>
